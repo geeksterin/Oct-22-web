6ff383b3fbaa3ae359bd143bee6d8ba6a5e3732e
Rishabh32700 <rishabhpep@gmail.com>
2022-11-02 22:04:23 +0530

    day 7 notes done

---
+++ DAY-007 (02-11-2022)/index.js
@@ -0,0 +1,60 @@
+// var key = "hello"
+// var num = 5
+
+// switch (num) {
+//     case num>5:
+//         console.log("greater");
+//         break;
+//     case num<5:
+//         console.log("lower");
+//         break;
+//     default:
+//         console.log("equal")
+//         break;
+// }
+
+// for(var i = 0; i<=5; i++){
+//     console.log("i inside", i)
+// }
+// console.log("i outside", i)
+
+// for(var i = 1; i>0; i++){
+//     console.log("i'm a good teacher");
+// }
+
+
+var a = 0
+var b = 5
+
+// while(a<=b){
+//     a++
+//     console.log("a is less", a);
+// }
+
+// var obj = {
+//     name: "Rishabh",
+//     age:"21",
+//     profession:"front-end dev",
+//     passion: "teaching"
+// }
+
+// var hello = 1
+// do{
+//     console.log("hello");
+// }while(typeof(hello)=="number")
+
+// console.log(typeof(hello));
+
+
+for(var x =0 ; x<5; x++){
+    if(x===3){
+        continue
+    }
+    console.log(x);
+}
+
+var bb = 10
+10 == bb
+10 === bb
+"10" == bb
+"10" === bb
