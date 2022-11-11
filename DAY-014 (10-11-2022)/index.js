18e7616bc804f8a649182c9759ead5ac72a7f41e
Rishabh32700 <rishabhpep@gmail.com>
2022-11-11 19:55:13 +0530

    day 15 content added day 14 noted pushed

---
+++ DAY-014 (10-11-2022)/index.js
@@ -0,0 +1,56 @@
+// var a = 10
+// arr = [1,2,3,54,67,]
+// arr[0]
+//  const obj = {
+//     "name2" : "rishabh",
+//     "arr" : [1,2,23,34,45,],
+//     name1: "afjhcasg"
+//  }
+
+//  obj.name2
+// obj["name2"]
+
+// var name = "abc"
+// var age = 20
+// const hello = {
+//     name : "rishabh",
+//     age: 21,
+//     getMyDetails: function () {
+//         return `"normal====>" ${this.name} ${this.age}`
+//     },
+//     myDetails2 :  () => {
+//         return `"arrow=====>" ${this.name} ${age}`
+//     }
+// }
+
+// console.log(hello.getMyDetails());
+// console.log(hello.myDetails2());
+// console.log(`${name}`);
+
+
+// hello.newKey = "newValue"
+// hello.dsjhvfgdsyf = [1,2,3]
+// hello.dsjhvfgdsyf.push(45)
+// hello.dflkrhjhwerou = {key1:"value1"}
+// hello.dflkrhjhwerou.key2 = "value2"
+// console.log(hello);
+
+var key 
+const hello = {
+    name : "rishabh",
+    ph:"98231748362",
+    key
+}
+
+// console.log(hello);
+const copiedObj = Object.assign(hello)
+
+// console.log( Object.values(hello));
+console.log( Object.entries(hello));
+console.log( hello.hasOwnProperty("sanjbcv"));
+
+
+
+
+
+
