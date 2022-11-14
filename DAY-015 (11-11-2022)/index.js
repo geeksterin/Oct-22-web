e138a6b2b41caf49e610d52e686945d4b6e73b3d
Rishabh32700 <rishabhpep@gmail.com>
2022-11-14 20:04:12 +0530

    day 16 topics added

---
+++ DAY-015 (11-11-2022)/index.js
@@ -0,0 +1,79 @@
+// const pie = Math.PI
+// c/
+// console.log(Math.round(6.5));
+// console.log(Math.round(5.4));
+
+// console.log(Math.floor(pie));
+// console.log(Math.floor(3.01));
+// console.log(Math.floor(3.15));
+// console.log(Math.floor(3.9));
+// console.log(Math.ceil(3.5));
+// console.log(Math.ceil(3.01));
+// console.log(Math.ceil(-1.5));
+// console.log(Math.ceil(pie));
+
+
+// console.log(Math.max(1,2,433,5,7,6, -667));
+
+// const random =( Math.random() * 6 + 1)
+// hello = (random * 10) - 4
+// console.log(Math.floor(hello));
+// console.log(Math.floor(random));
+
+// console.log(Math.abs(-10));
+
+// console.log(Math.sqrt(34.6));
+// console.log(Math.sqrt(34));
+
+// console.log(Math.pow(3,40)); 
+
+// console.log(Math.E);
+
+// console.log(Math.log(10));
+// console.log(Math.log(Math.E));
+
+// console.log(Math.sin(0));
+// console.log(Math.sin(90));
+
+const now1 = new Date()
+// const now = Date()
+// console.log(now);
+// console.log(now1);
+
+console.log(now1);
+console.log(now1);
+console.log(now1.getDate());
+console.log(now1.getTime());
+// console.log(typeof(now1));
+// console.log(now);
+// console.log(now1);
+
+
+let name = "rishabh"
+let age = 21
+let phone = new String("gdfgdfvhjs")
+let phone1 = new String(2)
+let phone2 = new String([1,2,3,45,6,7])
+let phone3 = new String({key:"hasudifhs"})
+let hello = "9" + 3
+let hey
+let ggg = null
+
+let arr = [1,2,34,45]
+let obj = {}
+console.log(arr[2]);
+console.log(hello);
+// console.log(phone[0]);
+// console.log(typeof(name));
+// console.log(typeof(age));
+// console.log(typeof(phone));
+// console.log(typeof(phone1));
+// console.log(typeof(phone2));
+// console.log(typeof(phone3));
+// console.log(typeof(arr));
+// console.log(typeof(obj));
+// console.log(typeof(hey));
+// console.log(typeof(ggg));
+// console.log(typeof(null));
+
+
