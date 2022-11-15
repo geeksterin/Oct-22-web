61c86b7a0ec79f260d0779c5df4b192dae4cff15
Rishabh32700 <rishabhpep@gmail.com>
2022-11-15 22:19:44 +0530

    day 17 notes

---
+++ DAY-017 (15-11-2022)/index.js
@@ -0,0 +1,65 @@
+// arr = ["abc", "def", ]
+
+// const ans = arr.every((ele)=>{
+//     return typeof ele === "string"
+// })
+
+// console.log(ans);
+
+// arr = [12,234,45,67,32,567]
+// const ans = arr.find((ele)=>{
+//     return ele>45
+// })
+// console.log(ans);
+
+// arr = [12,234,45,67,32,567]
+// const ans = arr.findIndex((ele)=>{
+//     return ele>45
+// })
+// console.log(ans);
+
+
+// arr = ["abc", "def", "rishabh", "geekster"]
+
+// const ans = arr.some((element)=>{
+//    return element.length == 4
+// })
+// console.log(ans);
+
+
+// console.log("start");
+
+// setTimeout(() => {
+//     console.log("setTimeOut1");
+// },1000);
+// setTimeout(() => {
+//     console.log("setTimeOut2");
+// },1000);
+
+// console.log("end");
+
+
+// console.log("start");
+
+// document.getElementById("button").
+//     addEventListener("click", function name() {
+//     console.log("button clicked");
+// })
+
+// console.log("end");
+
+
+console.log("start");
+
+setTimeout(() => {
+    console.log("setTimeOut1");
+},1000);
+
+fetch("https://python3-dot-parul-arena-2.appspot.com/test?cityname=New York").then(function name() {
+    console.log("get response");
+})
+
+console.log("end");
+
+
+
